841f8095effb899d1d5474ec431082e2e81d2f1d
LucasNBS <lucasnunesbarretosampaio@gmail.com>
2025-10-24 10:56:37 -0300

    feature: Adding patrimony page

--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, "pages/panel.html", {})
+
+def patrimony(request):
+  return render(request, "pages/patrimony.html", {})
