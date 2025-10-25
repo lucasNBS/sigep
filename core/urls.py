b4892fa7d3588e8ac9649a9104e8f171efc77071
LucasNBS <lucasnunesbarretosampaio@gmail.com>
2025-10-25 11:28:46 -0300

    feature: Adding records page

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import home
+from core.views import home, records
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('registro/', records),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, "pages/panel.html", {})
+
+def records(request):
+  return render(request, "pages/record.html", {})
