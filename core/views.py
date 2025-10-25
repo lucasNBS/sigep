dca190d68dc52d7772bed2b425899a209dc24b0e
LucasNBS <lucasnunesbarretosampaio@gmail.com>
2025-10-25 11:10:13 -0300

    feature: Adding inventory page

--- core/views.py
+++ core/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, "pages/panel.html", {})
+
+def inventory(request):
+  return render(request, "pages/inventory.html", {})
+
+def inventory_detail(request, id):
+  return render(request, "pages/inventory-detail.html", {})
