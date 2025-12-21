8644b559e0201659bacc8cd5bdd266372d090d0c
geigtoria <geisa04vitoria@gmail.com.com>
2025-12-21 17:42:31 -0300

    feat: Start of user CRUD

--- user/forms.py
+++ user/forms.py
@@ -1,1 +1,27 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.forms import inlineformset_factory
+
+from .models import Permission
+
+User = get_user_model()
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+            "photo_url",
+            "is_active",
+        )
+
+PermissionFormSet = inlineformset_factory(
+    parent_model=User,
+    model=Permission,
+    fields=("institution", "role"),
+    extra=1,
+    can_delete=True,
+)
--- user/views.py
+++ user/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView, View
 
-def user(request):
-  return render(request, "pages/user.html", {})
+from .forms import UserForm, PermissionFormSet
 
-def user_detail(request, id):
-  return render(request, "pages/user-detail.html", {})
+User = get_user_model()
+
 
 def login(request):
   return render(request, "pages/signin.html", {})
@@ -20,3 +24,77 @@ def reset_password(request):
 
 def new_password(request):
   return render(request, "pages/new-password.html", {})
+
+class UserListView(ListView):
+    model = User
+    template_name = "pages/user.html"
+    context_object_name = "users"
+    paginate_by = 20
+    ordering = ("username",)
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = "pages/user-detail.html"
+    context_object_name = "user_obj"
+
+
+class UserCreateView(View):
+    template_name = "pages/user-form.html"
+
+    def get(self, request):
+        form = UserForm()
+        formset = PermissionFormSet()
+        return render(request, self.template_name, {"form": form, "formset": formset})
+
+    @transaction.atomic
+    def post(self, request):
+        form = UserForm(request.POST, request.FILES)
+        formset = PermissionFormSet(request.POST)
+
+        if form.is_valid() and formset.is_valid():
+            user = form.save()
+            formset.instance = user
+            formset.save()
+            messages.success(request, "Usuário cadastrado com sucesso!")
+            return redirect("user:detail", pk=user.pk)
+
+        return render(request, self.template_name, {"form": form, "formset": formset})
+
+
+class UserUpdateView(View):
+    template_name = "pages/user-form.html"
+
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        form = UserForm(instance=user)
+        formset = PermissionFormSet(instance=user)
+        return render(
+            request,
+            self.template_name,
+            {"form": form, "formset": formset, "user_obj": user},
+        )
+
+    @transaction.atomic
+    def post(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        form = UserForm(request.POST, request.FILES, instance=user)
+        formset = PermissionFormSet(request.POST, instance=user)
+
+        if form.is_valid() and formset.is_valid():
+            form.save()
+            formset.save()
+            messages.success(request, "Usuário atualizado com sucesso!")
+            return redirect("user:detail", pk=user.pk)
+
+        return render(
+            request,
+            self.template_name,
+            {"form": form, "formset": formset, "user_obj": user},
+        )
+
+
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = "pages/user-delete.html"
+    success_url = reverse_lazy("user:list")
