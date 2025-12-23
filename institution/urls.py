412e2e9d92d18ff168795a7cbe021c4fcaeb98f7
J-Vicente <joaovicent2713@gmail.com>
2025-12-23 16:29:28 -0300

    feat: add institution dashboard list view

--- institution/urls.py
+++ institution/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import institution
+from .views import ListInstitutionsView
 
 urlpatterns = [
-    path('', institution, name='institution'),
+    path('', ListInstitutionsView.as_view(), name='dashboard'),
 ]
--- institution/views.py
+++ institution/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DetailView, ListView, DeleteView
+from . import models, forms
 
-def institution(request):
-  return render(request, "pages/panel.html", {})
+# remover quando o login estiver implementado
+from user import models as usermodel
+
+class ListInstitutionsView(ListView):
+  model = models.Institution
+  template_name = "institution/dashboard.html"
+  context_object_name = "institutions"
+
+  def get_queryset(self):
+    user = self.request.user
+
+    # remover quando o login estiver implementado
+    if not user.is_authenticated:
+      user = usermodel.User.objects.first()
+
+    user_institutions = models.Institution.objects.filter(permissions__user=user)
+
+    return user_institutions.distinct()
